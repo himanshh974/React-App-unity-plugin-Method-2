89f33b4a086ae5441c4758028690e4833a5898ef
Himanshu singh <60419105+himanshh974@users.noreply.github.com>
2020-06-01 15:45:31 +0530

    Add files via upload

---
+++ plugin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class fuck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    string somestring;
+    public void MsgToPlugin()
+    {
+
+
+        AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaObject bridge = new AndroidJavaObject(" com.lop.conne.plugin");
+        //AndroidJavaClass bridge = new AndroidJavaClass(" com.lop.conne.plugin");
+
+        bridge.CallStatic("blue");
+        //somestring = bridge.Call<string>("blue");
+        // Debug.Log(somestring);
+    }
+}
